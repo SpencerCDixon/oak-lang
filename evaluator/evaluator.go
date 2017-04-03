74c1d50ec1aebee3a293887d86a44c02b86c6fc7
Spencer Dixon <spencercdixon@gmail.com>
2017-04-03 09:47:52 -0400

    Adds support for calling functions and closures

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -68,8 +68,25 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
-	}
 
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		// evaluate all args and if there are any errors return err
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
+
+	}
 	return nil
 }
 
@@ -140,6 +157,21 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 }
 
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			// calling code checks first index for error
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+
+	return result
+}
+
 func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	if !isInteger(right) {
 		return newError("unknown operator: -%s", right.Type())
@@ -203,6 +235,33 @@ func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Obje
 		// default to always returning null
 		return NULL
 	}
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+	return obj
 }
 
 //----------
